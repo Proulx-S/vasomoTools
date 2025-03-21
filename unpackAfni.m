a3d82bc91607aae1f1a569507804fdc45d4ede17
Sebastien Proulx <proulxs@stanford.edu>
2025-03-21 00:13:02 -0700

    Fix on last fix

--- unpackAfni.m
+++ unpackAfni.m
@@ -290,7 +290,9 @@ for k = 1:fRes.param.dsgn.condK
 
             % Adjusting relative to principal vector
             rho   = abs(mriCoef.vol);                         % Magnitude
-            if ~nullFlag
+            if nullFlag
+                theta = angle(mriCoef.vol);
+            else
                 theta = wrapToPi(angle(mriCoef.vol) - angle(v));  % Phase
             end
             mriPol = mriCoef; mriPol.fspec = fOutPolar;
