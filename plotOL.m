46afb0b53843b577432bd476e5a63479ed0baacb
Sebastien Proulx <proulxs@stanford.edu>
2026-01-12 22:31:46 -0800

    bug fix

--- plotOL.m
+++ plotOL.m
@@ -19,11 +19,13 @@ function [hF,hAO,hIO] = plotOL(rCond,metric,roi,Hbase)
 
     %% Setup figure
     if usejava('desktop')
-        tiling.main.hF = figure('WindowStyle','docked');
+        % tiling.main.hF = figure('WindowStyle','docked');
+        hF = figure('WindowStyle','docked');
     else
-        tiling.main.hF = figure('MenuBar','none','ToolBar','none');
+        % tiling.main.hF = figure('MenuBar','none','ToolBar','none');
+        hF = figure('MenuBar','none','ToolBar','none');
     end
-    hF = figure('WindowStyle','docked');
+    % hF = figure('WindowStyle','docked');
     if isempty(roi)
     else
         hAO = cell(size(Hbase));
